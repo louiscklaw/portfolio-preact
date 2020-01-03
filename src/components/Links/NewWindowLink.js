d8a88ac25e8c8fb3996cb91dc67d6bcb2736a536
louiscklaw <louiscklaw@gmail.com>
2020-01-03 23:37:54 +0800

    update to tidy code,

--- src/components/Links/NewWindowLink.js
+++ src/components/Links/NewWindowLink.js
@@ -1,10 +1,10 @@
 import style from './NewWindowLink.scss'
 
-import {Component} from 'preact';
+import { Component } from 'preact';
 
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 
-export default ({link, text}) => {
+export default ({ link, text }) => {
   return (
     <div className={style.NewWindowLink}>
       <a href={link} target="_blank" className={style.WindowLink} rel="noopener noreferrer" >
