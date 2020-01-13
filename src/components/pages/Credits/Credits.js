87aba48093647ef31230c7eaeed754daab3c36cd
louiscklaw <lousicklaw@gmail.com>
2020-01-13 15:55:10 +0800

    update for credit page,

--- src/components/pages/Credits/Credits.js
+++ src/components/pages/Credits/Credits.js
@@ -1,12 +1,41 @@
 import { Component } from 'preact';
+
 import style from './Credits.scss';
 
 export default class Credits extends Component {
   render(){
     return(
-      <div>
-        <div className={style.helloworld}>
+      <div className={style.page_credit}>
+
+        <div className={style.page_header}>
           Credits
+        </div>
+
+        <div className={style.paragraph_body}>
+          <section>
+            <h4>Tools:</h4>
+            <ul>
+              <li>
+                <a href="https://github.com/preactjs/preact">preact</a>
+              </li>
+              <li>
+                <a href="https://github.com/FortAwesome/Font-Awesome">font-awesome</a>
+              </li>
+            </ul>
+          </section>
+
+          <section>
+            <h4>opensource projects:</h4>
+            <ul>
+              <li>
+                preact, react
+              </li>
+              <li>
+                fontawesome
+              </li>
+            </ul>
+          </section>
+
         </div>
       </div>
     )
