98d456fb367b56882f1be84a4cfa0073334e9e3c
louiscklaw <lousicklaw@gmail.com>
2020-01-13 22:30:45 +0800

    update,

--- src/components/pages/ProjectDetail/PetPugProject/PetPugProject.js
+++ src/components/pages/ProjectDetail/PetPugProject/PetPugProject.js
@@ -1,3 +1,5 @@
+import projectDetailStyle from '../projectdetail.scss';
+
 import {Component} from 'preact';
 
 import {
@@ -7,21 +9,83 @@ import {
   YoutubeContainer,
   MetaTitle,
   GithubLink,
-  MetaProjectDetail
+  MetaProjectDetail,
+  HeaderFigure,
+  Spare,
+  ProjectDetailHelmets,
+  PrismHelloworld,
+  PrismBash
 } from '../../../../components/index'
 
 // import './ProjectDescription.css';
 // import './TradingviewDashboard.css';
 
-// import head_pic from './appium-behave-notes.png'
+import head_pic from './thumbnail.png'
 
 import PetPugProjectThumbnail from './thumbnail.png';
 
 class PetPugProjectContent extends Component{
   render(){
     return(
-      <div>
-        PetPugProject behave notes
+      <div className={projectDetailStyle.Container}>
+
+        <ProjectDetailHelmets
+          title="Pet pug project"
+          description="A Pet pug project"
+          image={PetPugProjectThumbnail}
+        />
+
+        <div className={projectDetailStyle.ProjectDescription}>
+
+          <div className={projectDetailStyle.ProjectHead}>
+            <BackToProjects />
+            <ProjectTitle
+              project_title="Appium driven by behave tryout"
+            />
+            <Spare />
+          </div>
+
+          <HeaderFigure
+            img_src={head_pic}
+            caption="a pet pug project"
+          />
+
+
+          <div className={projectDetailStyle.DescBody}>
+            <h3 className={projectDetailStyle.DescTopic}>
+              Purpose:
+            </h3>
+            <p>
+              My pet projects
+            </p>
+
+            <h3 className={projectDetailStyle.DescTopic}>
+              Repo:
+            </h3>
+            <ul>
+              <li>
+                <NewWindowLink link="https://github.com/louiscklaw/pug-startbootstrap-sb-admin-2" />
+              </li>
+              <li>
+                <NewWindowLink link="https://github.com/louiscklaw/pug_hugo_slate" />
+              </li>
+              <li>
+                <NewWindowLink link="https://github.com/louiscklaw/pug_landing_page" />
+              </li>
+              <li>
+                <NewWindowLink link="https://github.com/louiscklaw/pug-startbootstrap-coming-soon" />
+              </li>
+              <li>
+                <NewWindowLink link="https://github.com/louiscklaw/pug-stylish-portfolio" />
+              </li>
+            </ul>
+
+
+
+          </div>
+
+        </div>
+
       </div>
     )
   }
