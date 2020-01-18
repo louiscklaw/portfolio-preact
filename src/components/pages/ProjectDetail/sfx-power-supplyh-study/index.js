386da411a4af72739266dc66547523ef4031ee02
Travis CI <travis@travis-ci.org>
2020-01-18 13:26:45 +0800

    init commit,

---
+++ src/components/pages/ProjectDetail/sfx-power-supplyh-study/index.js
@@ -0,0 +1,34 @@
+import projectDetailStyle from '../projectdetail.scss';
+
+import {Component} from 'preact';
+
+import {
+  ProjectTitle,
+  NewWindowLink,
+  BackToProjects,
+  YoutubeContainer,
+  MetaTitle,
+  GithubLink,
+  GistLink,
+  MetaProjectDetail,
+  HeaderFigure,
+  Spare,
+  ProjectDetailHelmets,
+  PrismHelloworld,
+  PrismBash
+} from '../../../../components/index'
+
+import head_pic from './src/PS-STP-0600FPCGXX-G_d061bebe05a4488c96fe855efa35f9b3.jpg';
+
+import Thumbnail from './thumbnail.jpg';
+
+
+export default class PostContent extends Component{
+  render(){
+    return(
+      <div>
+        hello sfx power supply
+      </div>
+    )
+  }
+}
