b0c9fb577397e981509db3bce9182d2695a24727
Travis CI <travis@travis-ci.org>
2020-01-21 16:36:43 +0800

    update for top menu,

--- src/components/Menu/TopBar.js
+++ src/components/Menu/TopBar.js
@@ -5,25 +5,32 @@ import theme from '../../style/theme'
 import { faFileExcel } from '@fortawesome/free-regular-svg-icons';
 
 export default class TopBar extends Component{
-  handleOnScroll(){
+
+  handleOnScroll(sticky_menu_trigger_px){
     console.log('capture onscroll')
     console.log(window.pageYOffset);
     var ele_top_menu_body = document.querySelector('.top-menu-body')
 
-    if (window.pageYOffset > 91){
-      ele_top_menu_body.style.backgroundColor = "green"
+    if (window.pageYOffset > sticky_menu_trigger_px){
+      // ele_top_menu_body.style.backgroundColor = "green"
       ele_top_menu_body.style.top= '0px'
       ele_top_menu_body.style.position = 'fixed';
+      ele_top_menu_body.style.background = 'linear-gradient(180deg, rgba(39,60,117,1) 0%, rgba(39,60,117,1) 60%, rgba(39,60,117,0) 100%)'
+      // ele_top_menu_body.style.background="linear-gradient(180deg, rgba(39,60,117,1) 0%, rgba(39,60,117,1) 60%, rgba(39,60,117,0) 100%)";
+      // ele_top_menu_body.style.backgroundColor="red";
 
     }else{
-      ele_top_menu_body.style.backgroundColor = "gold"
+      // ele_top_menu_body.style.backgroundColor = "gold"
       ele_top_menu_body.style.top= 'unset'
       ele_top_menu_body.style.position = 'unset';
+      ele_top_menu_body.style.background = 'unset';
     }
-
   }
+
   componentDidMount(){
-    window.onscroll = this.handleOnScroll
+    window.addEventListener('scroll',(event) => {
+      this.handleOnScroll(document.querySelector('.top-menu-body').offsetTop)
+    })
   }
 
   render(){
@@ -36,19 +43,18 @@ export default class TopBar extends Component{
         textAlign: 'center'
       },
       narrow_menu:{
+        paddingTop: '2em',
+
         display:'flex',
         justifyContent: 'space-evenly',
         width: '100%',
         listStyle: 'none',
-        color: theme.THEME_COLOR_LIGHTEST,
-
-        backgroundColor: 'red'
+        color: theme.THEME_COLOR_LIGHTEST
       },
       greeting_container:{
 
       },
       narrow_menu_container:{
-        paddingTop: '2em',
         height: '10vh',
         width: '100%',
         zIndex: '2'
@@ -65,19 +71,20 @@ export default class TopBar extends Component{
         </div>
 
         <div style={style.narrow_menu_container} className="top-menu-body">
-          <ul style={style.narrow_menu}>
-            <li>
-              <a style={style.a} href="/project_catalogue" >Projects</a>
-            </li>
 
-            <li>
-              <a style={style.a} href="/about" >About</a>
-            </li>
+            <ul style={style.narrow_menu}>
+              <li>
+                <a style={style.a} href="/project_catalogue" >Projects</a>
+              </li>
 
-            <li>
-              <a style={style.a} href="/credits">Credits</a>
-            </li>
-          </ul>
+              <li>
+                <a style={style.a} href="/about" >About</a>
+              </li>
+
+              <li>
+                <a style={style.a} href="/credits">Credits</a>
+              </li>
+            </ul>
 
         </div>
       </div>
