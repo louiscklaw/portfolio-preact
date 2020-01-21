b0a40056a7c86df9fcfca79ad2132ea32358b92f
Travis CI <travis@travis-ci.org>
2020-01-21 15:59:53 +0800

    in the middle, the sticky working,

--- src/components/Menu/TopBar.js
+++ src/components/Menu/TopBar.js
@@ -5,6 +5,27 @@ import theme from '../../style/theme'
 import { faFileExcel } from '@fortawesome/free-regular-svg-icons';
 
 export default class TopBar extends Component{
+  handleOnScroll(){
+    console.log('capture onscroll')
+    console.log(window.pageYOffset);
+    var ele_top_menu_body = document.querySelector('.top-menu-body')
+
+    if (window.pageYOffset > 91){
+      ele_top_menu_body.style.backgroundColor = "green"
+      ele_top_menu_body.style.top= '0px'
+      ele_top_menu_body.style.position = 'fixed';
+
+    }else{
+      ele_top_menu_body.style.backgroundColor = "gold"
+      ele_top_menu_body.style.top= 'unset'
+      ele_top_menu_body.style.position = 'unset';
+    }
+
+  }
+  componentDidMount(){
+    window.onscroll = this.handleOnScroll
+  }
+
   render(){
     const style = {
       hello_greeting:{
@@ -28,7 +49,9 @@ export default class TopBar extends Component{
       },
       narrow_menu_container:{
         paddingTop: '2em',
-        height: '10vh'
+        height: '10vh',
+        width: '100%',
+        zIndex: '2'
       },
       a :{
         color: theme.THEME_COLOR_LIGHTEST
@@ -41,7 +64,7 @@ export default class TopBar extends Component{
           Hi, I am louis
         </div>
 
-        <div style={style.narrow_menu_container}>
+        <div style={style.narrow_menu_container} className="top-menu-body">
           <ul style={style.narrow_menu}>
             <li>
               <a style={style.a} href="/project_catalogue" >Projects</a>
