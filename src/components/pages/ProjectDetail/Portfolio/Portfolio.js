29b61cde410f84039bfa47a53584d8a026ea6768
Travis CI <travis@travis-ci.org>
2020-01-21 17:00:21 +0800

    working check point, extract the scss to js,

--- src/components/pages/ProjectDetail/Portfolio/Portfolio.js
+++ src/components/pages/ProjectDetail/Portfolio/Portfolio.js
@@ -1,3 +1,4 @@
+import projectDetailStyle from '../projectDetailStyle';
 import {Component} from 'preact';
 
 import {
@@ -7,21 +8,59 @@ import {
   YoutubeContainer,
   MetaTitle,
   GithubLink,
-  MetaProjectDetail
+  GistLink,
+  MetaProjectDetail,
+  HeaderFigure,
+  Spare,
+  ProjectDetailHelmets,
+  PrismHelloworld,
+  PrismBash
 } from '../../../../components/index'
 
 // import './ProjectDescription.css';
 // import './TradingviewDashboard.css';
 
-// import head_pic from './appium-behave-notes.png'
-
+import head_pic from './screen_capture.jpeg'
 import PortfolioThumbnail from './thumbnail.png';
 
 class PortfolioContent extends Component{
+
   render(){
+    console.log(projectDetailStyle);
     return(
-      <div>
-        Portfolio behave notes
+      <div style={projectDetailStyle.Container}>
+
+        <ProjectDetailHelmets
+          title="My portfolio page"
+          description="My portfolio page"
+          image={PortfolioThumbnail}
+        />
+
+        <div style={projectDetailStyle.ProjectDescription}>
+
+          <div style={projectDetailStyle.ProjectHead}>
+            <BackToProjects />
+            <ProjectTitle
+              project_title="Making of my portfolio page"
+            />
+            <Spare />
+          </div>
+
+          <HeaderFigure
+            img_src={head_pic}
+            caption="screenshot of my portfolio page"
+          />
+
+          <div style={projectDetailStyle.DescBody}>
+            <h3 style={projectDetailStyle.DescTopic}>
+              Purpose:
+            </h3>
+            <p>
+              create a pcb for voron printer.
+            </p>
+
+          </div>
+        </div>
       </div>
     )
   }
---
+++ src/components/pages/ProjectDetail/projectDetailStyle.js
@@ -0,0 +1,52 @@
+import theme from '../../../style/theme'
+
+var Container = {
+  color: theme.THEME_COLOR,
+  minHeight: '100vh'
+}
+
+
+var ProjectHead ={
+  display: "flex",
+  flexDirection: "row",
+  justifyContent: "spaceBetween",
+}
+
+var a = {
+  color: "rgba(52, 73, 94, 1.0)",
+}
+
+var ProjectDescription = {
+  marginTop: "4em",
+  marginBottom: "4em",
+  marginLeft: "auto",
+  marginRight: "auto",
+  width: "75%",
+  textAlign: "left",
+}
+var DescBody = {
+  paddingTop: "1.1em",
+  paddingBottom: "1.3em",
+}
+var DescTopic = {
+  fontWeight: "700",
+  paddingTop: "2em",
+  paddingBottom: "0.5em",
+
+  textDecoration: "underline",
+}
+
+var ul = {
+  listStyle: "none",
+}
+
+
+export default {
+  Container,
+  a,
+  ProjectDescription,
+  DescBody,
+  DescTopic,
+  ul,
+  ProjectHead
+}
