fbae53cff733ca8e95f1320ab63d01b274c0f870
louiscklaw <louiscklaw@gmail.com>
2020-02-16 14:19:06 +0800

    fix(build-info): fix hardcoded in template,

--- src/build_info.js
+++ src/build_info.js
@@ -1,4 +1,6 @@
-const BUILD_VER = "<<BUILD_VER>>";
-const BUILD_DATE = "<<BUILD_DATE>>";
+const BUILD_VER = "";
+const BUILD_DATE = "2020-Feb-16";
 
 export { BUILD_VER, BUILD_DATE };
+
+// NOTE: generated from scripts/build_info.js.template, do not modify this file
