098fe497e63b6187e922c05d47f0761ca4c3db33
louiscklaw <louiscklaw@gmail.com>
2020-02-26 16:11:17 +0800

    ci: update commitlint rules,

--- commitlint.config.js
+++ commitlint.config.js
@@ -7,4 +7,26 @@ LICENSE file in the root directory of this source tree.
 
 // @flow
 /*eslint-env node*/
-module.exports = { extends: ["@commitlint/config-conventional"] };
+module.exports = {
+  extends: ["@commitlint/config-conventional"],
+  rules: {
+    "type-enum": [
+      2,
+      "always",
+      [
+        "build",
+        "chore",
+        "ci",
+        "docs",
+        "feat",
+        "fix",
+        "perf",
+        "refactor",
+        "revert",
+        "style",
+        "test",
+        "content"
+      ]
+    ]
+  }
+};
