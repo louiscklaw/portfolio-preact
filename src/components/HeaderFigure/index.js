d6e860cb90b22455774d7cae7a65d3c81bf71ed3
louiscklaw <louiscklaw@gmail.com>
2020-03-01 21:03:36 +0800

    feat: update for handle the figure_style from page detail,

--- src/components/HeaderFigure/index.js
+++ src/components/HeaderFigure/index.js
@@ -2,11 +2,11 @@ import style from "./style.scss";
 
 import { Component } from "preact";
 
-export default ({ img_src, caption }) => {
+export default ({ img_src, caption, figure_style }) => {
   return (
     <div className={style.HeaderFigureContainer}>
       <figure>
-        <img src={img_src} alt="" />
+        <img src={img_src} alt="" style={figure_style} />
       </figure>
       <figcaption className={style.HeaderFigureCaption}>{caption}</figcaption>
     </div>
