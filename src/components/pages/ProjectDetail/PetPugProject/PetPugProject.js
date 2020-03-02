e5b4b758e063f35956daa965a715048719301a98
louiscklaw <louiscklaw@gmail.com>
2020-03-02 20:53:50 +0800

    contents: update contents of pet project,

--- src/components/pages/ProjectDetail/PetPugProject/PetPugProject.js
+++ src/components/pages/ProjectDetail/PetPugProject/PetPugProject.js
@@ -44,12 +44,32 @@ class PetPugProjectContent extends Component {
           <HeaderFigure img_src={head_pic} caption="Pet pug project" />
 
           <div className={projectDetailStyle.DescBody}>
-            <h3 className={projectDetailStyle.DescTopic}>Linux toolbox:</h3>
-            <p>ack, entr, inotify-tools</p>
-
-            <h3 className={projectDetailStyle.DescTopic}>References:</h3>
+            <h3 className={projectDetailStyle.DescTopic}>Description:</h3>
+            <p>Some pet software project from me</p>
 
             <ul>
+              <li>
+                <GithubLink link="https://github.com/louiscklaw/google-search-trends-v3" />
+              </li>
+              <li>
+                <GithubLink link="https://github.com/louiscklaw/test-bulma-firebase-crud" />
+              </li>
+              <li>
+                <GithubLink link="https://github.com/louiscklaw/bulma-admin-templates" />
+              </li>
+              <li>
+                <GithubLink link="https://github.com/louiscklaw/bulma-firebase-crud-tryout" />
+              </li>
+            </ul>
+
+            <h3 className={projectDetailStyle.DescTopic}>
+              Awesome Lists of the world:
+            </h3>
+            <p>Some highlighted awesome list</p>
+            <ul>
+              <li>
+                <GithubLink link="https://github.com/alexpate/awesome-design-systems" />
+              </li>
               <li>
                 <GithubLink link="https://github.com/jenil/bulmaswatch" />
               </li>
