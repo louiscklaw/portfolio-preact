2ad0b59cd22b72f0a36a474404444298a9cf50e4
louiscklaw <louiscklaw@gmail.com>
2020-04-07 20:50:23 +0800

    contents: update for using GithubLinkList,

--- src/components/pages/ProjectDetail/sfx-power-supply-study/index.js
+++ src/components/pages/ProjectDetail/sfx-power-supply-study/index.js
@@ -15,7 +15,8 @@ import {
   Spare,
   ProjectDetailHelmets,
   PrismHelloworld,
-  PrismBash
+  PrismBash,
+  GithubLinkList,
 } from "../../../../components/index";
 
 import head_pic from "./src/PS-STP-0600FPCGXX-G_d061bebe05a4488c96fe855efa35f9b3.jpg";
@@ -49,15 +50,12 @@ class SFXPowerSupplyContent extends Component {
           {/*  */}
 
           <div className={projectDetailStyle.DescBody}>
-            <h3 className={projectDetailStyle.DescTopic}>Repos:</h3>
-            <ul>
-              <li>
-                <GithubLink link="https://github.com/louiscklaw/sfx-power-supply-playlist.git" />
-              </li>
-              <li>
-                <GithubLink link="https://github.com/louiscklaw/lichee-nano-one-key-package" />
-              </li>
-            </ul>
+            <GithubLinkList
+              github_links={[
+                "https://github.com/louiscklaw/sfx-power-supply-playlist.git",
+                "https://github.com/louiscklaw/lichee-nano-one-key-package",
+              ]}
+            />
           </div>
         </div>
       </div>
