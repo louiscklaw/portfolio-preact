64cfc8802bdaae87afec468b402b704e7ff1c1db
louiscklaw <louiscklaw@gmail.com>
2020-04-07 21:21:08 +0800

    ci: update for using new merging script and build step,

--- scripts/pre-push.sh
+++ scripts/pre-push.sh
@@ -1,7 +1,19 @@
 #!/usr/bin/env bash
 
 pretty-quick --staged
-
 rm -rf build
-
 yarn build
+
+set -ex
+
+for l in $(find . -name .travis.yml -not -path "./node_modules/*");
+do
+  travis lint $l &
+done;
+
+for l in $(ls -1 ./.travis/*.yml);
+do
+  travis lint $l &
+done;
+
+wait
