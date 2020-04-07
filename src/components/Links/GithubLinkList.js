ed9a619e147c379017d1d57d76273e26084c876c
louiscklaw <louiscklaw@gmail.com>
2020-04-07 20:05:52 +0800

    ci: update for GithubLinkList style,

--- src/components/Links/GithubLinkList.js
+++ src/components/Links/GithubLinkList.js
@@ -1,12 +1,13 @@
 import React, { Component } from "react";
 
 import GithubLink from "./GithubLink";
+import style from "./GithubLinkList.scss";
 
 export default class GithubLinkList extends Component {
   render({ github_links }) {
     return (
-      <>
-        <h3>References:</h3>
+      <div className={style.GithubLinkList}>
+        <h3 className={style.DescTopic}>References / Repositories:</h3>
         <ul>
           {github_links.map(x => (
             <li>
@@ -14,7 +15,7 @@ export default class GithubLinkList extends Component {
             </li>
           ))}
         </ul>
-      </>
+      </div>
     );
   }
 }
