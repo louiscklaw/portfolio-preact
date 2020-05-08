e82e9cf39e0d3d6ea5d7e1c631d202e3abf85124
louiscklaw <louiscklaw@gmail.com>
2020-05-08 12:51:19 +0800

    feat: update nav bar due to not enough space,

--- src/components/Menu/style.js
+++ src/components/Menu/style.js
@@ -2,53 +2,53 @@ import theme from "../../style/theme.js";
 
 var nav_middle = {
   paddingTop: "1em",
-  height: "33vh"
+  minHeight: "33vh",
 };
 
 var nav_middle_li = {
   textTransform: "uppercase",
   paddingTop: "10px",
   paddingBottom: "10px",
-  textAlign: "center"
+  textAlign: "center",
 };
 
 var a = {
   color: theme.THEME_COLOR_LIGHTEST,
-  fontWeight: "bold"
+  fontWeight: "bold",
 };
 
 var ul = {
-  listStyle: "none"
+  listStyle: "none",
 };
 
 var nav_bottom = {
   paddingTop: "2em",
-  height: "34vh"
+  // height: "34vh"
 };
 
 var credits_link_container = {
   paddingTop: "1px",
-  paddingBottom: "20px"
+  paddingBottom: "20px",
 };
 
 var credits_link = {
   textDecoration: "underline",
   marginTop: "1px",
   fontWeight: "bold",
-  color: theme.THEME_COLOR_LIGHTEST
+  color: theme.THEME_COLOR_LIGHTEST,
 };
 
 var build_status = {
   paddingTop: "10px",
   paddingBottom: "10px",
-  textAlign: "center"
+  textAlign: "center",
 };
 
 var source_code_link_container = {
   textAlign: "center",
   width: "100%",
   fontSize: "0.9em",
-  color: theme.THEME_COLOR_LIGHTEST
+  color: theme.THEME_COLOR_LIGHTEST,
 };
 
 var source_code_link = {
@@ -56,24 +56,24 @@ var source_code_link = {
   marginTop: "20px",
   fontSize: "0.8em",
   fontWeight: "bold",
-  color: theme.THEME_COLOR_LIGHTEST
+  color: theme.THEME_COLOR_LIGHTEST,
 };
 
 var credit_container = {
   paddingTop: "30px",
   color: theme.THEME_COLOR_LIGHTEST,
   fontSize: "0.8em",
-  textAlign: "center"
+  textAlign: "center",
 };
 
 var credit_container_a = {
   position: "relative",
-  listStyle: "none"
+  listStyle: "none",
 };
 
 var social_links = {
   display: "inline-flex",
-  listStyle: "none"
+  listStyle: "none",
 };
 
 var social_link_a = {
@@ -82,25 +82,25 @@ var social_link_a = {
   fontWeight: "normal",
   paddingLeft: "3px",
   paddingRight: "3px",
-  paddingBottom: "10px"
+  paddingBottom: "10px",
 };
 
 var social_links_container = {
   width: "100%",
-  textAlign: "center"
+  textAlign: "center",
 };
 
 var nav_container = {
-  backgroundColor: theme.THEME_COLOR
+  backgroundColor: theme.THEME_COLOR,
 };
 
 var mobile_menu = {
   display: "none",
-  backgroundColor: theme.THEME_COLOR
+  backgroundColor: theme.THEME_COLOR,
 };
 
 var menu_body = {
-  display: "block"
+  display: "block",
   // backgroundColor: 'red'
 };
 
@@ -133,5 +133,5 @@ export default {
   social_links,
   social_link_a,
   social_links_container,
-  credits_link_container
+  credits_link_container,
 };
