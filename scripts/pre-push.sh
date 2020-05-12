31c54cdacf15942a9a58d5271fe9bfe3da798cc3
louiscklaw <louiscklaw@gmail.com>
2020-05-12 16:12:53 +0800

    ci: simplify pre-push step,

--- scripts/pre-push.sh
+++ scripts/pre-push.sh
@@ -1,14 +1,1 @@
 #!/usr/bin/env bash
-
-pretty-quick --staged
-rm -rf build
-yarn build
-
-set -ex
-
-for l in $(find . -name .travis.yml -not -path "./node_modules/*");
-do
-  travis lint $l &
-done;
-
-wait
