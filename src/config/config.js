eb47dcbf55a8284cbe30cbc75ae14baf19037a19
louiscklaw <louiscklaw@gmail.com>
2020-05-13 15:33:26 +0800

    test: update to simplify CHECK_DEV_ENV,

--- src/config/config.js
+++ src/config/config.js
@@ -1,19 +1,11 @@
 import DEV_CONFIG from "./dev.config";
 import PROD_CONFIG from "./prod.config";
 
-var active_config = PROD_CONFIG;
-
-if (!process.env.NODE_ENV || process.env.NODE_ENV === "development") {
-  // dev code
-  console.warn("activate dev config");
-  active_config = DEV_CONFIG;
-} else {
-  // production code
-}
-
 const CHECK_DEV_ENV = () => {
   // true => development, false => production
   return process.env.NODE_ENV === "development";
 };
 
-export { active_config, CHECK_DEV_ENV };
+const active_config = CHECK_DEV_ENV ? DEV_CONFIG : PROD_CONFIG;
+
+export { CHECK_DEV_ENV, active_config };
--- src/index.js
+++ src/index.js
@@ -5,8 +5,7 @@ import ThemeSetting from "./style/theme";
 
 import Main from "./components/pages/Main/main";
 
-import DevConfig from "./config/dev.config";
-import ProdConfig from "./config/prod.config";
+import { active_config } from "./config/config";
 
 import ReactGA from "react-ga";
 
@@ -20,30 +19,18 @@ import { fas } from "@fortawesome/free-solid-svg-icons";
 import { fab } from "@fortawesome/free-brands-svg-icons";
 library.add(fab, fas, far);
 
-const CHECK_DEV_ENV = () => {
-  // true => development, false => production
-  let check_result = process.env.NODE_ENV === "development";
-  if (check_result) {
-    console.warn("check running environment.");
-  }
-  return check_result;
-};
-
-// get the final configuration by run environment
-const ACTIVE_CONFIG = CHECK_DEV_ENV() ? DevConfig : ProdConfig;
-
 export default class App extends Component {
   constructor(props) {
     super(props);
     this.state = {
       windowInnerWidth: 0,
-      windowInnerHeight: 0
+      windowInnerHeight: 0,
     };
     this.updateDimensions = this.updateDimensions.bind(this);
   }
 
   componentDidMount() {
-    ReactGA.initialize(ACTIVE_CONFIG.GAKey);
+    ReactGA.initialize(active_config.GAKey);
     ReactGA.pageview(window.location.pathname + window.location.search);
 
     this.updateDimensions();
@@ -67,7 +54,7 @@ export default class App extends Component {
 
     this.setState({
       windowInnerWidth,
-      windowInnerHeight
+      windowInnerHeight,
     });
   }
   render() {
@@ -80,7 +67,7 @@ export default class App extends Component {
       windowInnerHeight,
       isMobile: this.isMobile(),
       is_mobile: this.isMobile(),
-      currTheme: ThemeSetting
+      currTheme: ThemeSetting,
     };
 
     const build_info = { BUILD_VER, BUILD_DATE };
