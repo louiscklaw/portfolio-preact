bfbe20a2deaee9635596e135e1a7dce3fccc01a4
louiscklaw <louiscklaw@gmail.com>
2020-05-13 15:24:45 +0800

    feat: seperate color on nav bar for dev site,

--- src/style/theme.js
+++ src/style/theme.js
@@ -1,6 +1,14 @@
 import _util from "./util";
 
-const THEME_COLOR = `#273c75`;
+const CHECK_DEV_ENV = () => {
+  // true => development, false => production
+  return process.env.NODE_ENV === "development";
+};
+
+const PRODUCTION_THEME_COLOR = "#273c75";
+const DEVELOPMENT_THEME_COLOR = "#2c3e50";
+
+const THEME_COLOR = true ? DEVELOPMENT_THEME_COLOR : PRODUCTION_THEME_COLOR;
 
 const THEME_COLOR_LIGHTEST = _util.ColorLuminance(THEME_COLOR_LIGHTER, 0.2);
 const THEME_COLOR_LIGHTER = _util.ColorLuminance(THEME_COLOR, 0.2);
@@ -16,5 +24,5 @@ export default {
   THEME_COLOR_DARKER,
   THEME_COLOR_DARKEST,
 
-  DEFAULT_LINK_COLOR
+  DEFAULT_LINK_COLOR,
 };
